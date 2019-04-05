ab4d4734f60c94f5e72b58898dbd6b9e42e361ba
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2019-04-05 15:42:29 -0400

    Loop over each log files in the SD card

--- source/sdcard.c
+++ source/sdcard.c
@@ -8,15 +8,16 @@ int sdcardInit() {
     FRESULT result;
 
     result = f_mount(&filesystem, "", 1);
-    // TODO: this isn't returning FR_OK, needs to be debugged
     if(result != FR_OK) return -1;
 
     result = f_findfirst(&directory, &fileInfo, "/", "log*");
     if(result != FR_OK) return -1;
 
-    while(fileInfo.fname[0] == '\0') {
+    while(fileInfo.fname[0] != '\0') {
         result = f_findnext(&directory, &fileInfo);
         if(result != FR_OK) return -1;
+
+        // TODO: the next N where the file "logN" doesn't exist
     }
 
     return 0;
