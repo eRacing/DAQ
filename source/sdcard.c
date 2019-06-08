a75d256647c09132bd7d01218d8a6c7bf4b38e2d
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2019-06-08 14:27:01 -0400

    Various fixes on the SD card code

--- source/sdcard.c
+++ source/sdcard.c
@@ -12,10 +12,9 @@
 //////////
 
 static FATFS filesystem;
-static DIR directory;
 static FIL logfile;
 
-#define PREFIX_FILE "log"
+#define PREFIX_FILE "LOG"
 #define PREFIX_SIZE 3
 
 #define FILENAME_MAX_LENGTH 20
@@ -24,17 +23,18 @@ static FIL logfile;
 // Private Functions //
 ///////////////////////
 
-static int findNextLogFile(DIR* directory) {
+static int findNextLogFile() {
     FILINFO fileInfo;
     FRESULT result;
-
-    result = f_findfirst(directory, &fileInfo, "/", PREFIX_FILE "*");
-    if(result != FR_OK) return -1;
-
+    DIR directory;
     int logMaxNumber = 0;
+
+    result = f_findfirst(&directory, &fileInfo, "/", PREFIX_FILE "*");
+    if(result != FR_OK) goto fatfs_error;
+
     while(fileInfo.fname[0] != '\0') {
-        result = f_findnext(directory, &fileInfo);
-        if(result != FR_OK) return -1;
+        result = f_findnext(&directory, &fileInfo);
+        if(result != FR_OK) goto close_dir;
 
         int logNumber;
         if(sscanf(fileInfo.fname, PREFIX_FILE "%d", &logNumber) != 1) {
@@ -50,16 +50,40 @@ static int findNextLogFile(DIR* directory) {
         logMaxNumber = -logMaxNumber;
     }
 
-    return logMaxNumber;
+    result = f_closedir(&directory);
+    if(result != FR_OK) goto fatfs_error;
+
+    return logMaxNumber+1;
+
+close_dir:
+    f_closedir(&directory);
+fatfs_error:
+    return result;
 }
 
 static int openLogFile(int logNumber, FIL* logFile) {
     char filename[FILENAME_MAX_LENGTH];
-    if(snprintf(filename, FILENAME_MAX_LENGTH, PREFIX_FILE "%d", logNumber) < 0) {
+    if(snprintf(filename, FILENAME_MAX_LENGTH, "/" PREFIX_FILE "%d", logNumber) < 0) {
         return -1;
     }
 
     FRESULT result = f_open(logFile, filename, FA_WRITE | FA_CREATE_ALWAYS);
+    if(result != FR_OK) {
+        return -1;
+    }
+
+    return 0;
+}
+
+static int remount(FATFS* filesystem) {
+    FRESULT result;
+
+    result = f_mount(0, "", 0);
+    if(result != FR_OK) {
+        return -1;
+    }
+
+    result = f_mount(filesystem, "", 1);
     if(result != FR_OK) {
         return -1;
     }
@@ -80,8 +104,17 @@ int sdcardInit() {
         return -1;
     }
 
-    int logfileNumber = findNextLogFile(&directory);
+    int logfileNumber = findNextLogFile();
     if(logfileNumber < 0) {
+        return -1;
+    }
+
+    /*
+     * For some reason, fatfs refuses to open the log file after
+     * the search done by findNextLogFile(). We fix this by
+     * remounting the filesystem.
+     */
+    if(remount(&filesystem) < 0) {
         return -1;
     }
 
@@ -94,9 +127,16 @@ int sdcardInit() {
 
 int sdcardWrite(void* data, size_t len) {
     size_t written;
-    FRESULT result = f_write(&logfile, data, len, &written);
+    FRESULT result;
+
+    result = f_write(&logfile, data, len, &written);
     if(result != FR_OK) {
         return -1;
+    }
+
+    result = f_sync(&logfile);
+    if(result != FR_OK) {
+         return -1;
     }
 
     return 0;
