6af4d4554276651847f3dfa44234329e296b64f3
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2019-06-09 10:43:09 -0400

    Add basic assertion in stress-test.sh

--- scripts/stress-test.sh
+++ scripts/stress-test.sh
@@ -2,7 +2,36 @@
 
 SCRIPT_NAME=$(basename "$0")
 SUBBUF_SIZE=32
+EVENT_SIZE=16
 CAN_DEVICE=can0
+
+function assert_eq()
+{
+    local msg=$1
+    local expected=$2
+    local got=$3
+
+    if [[ ! "$expected" == "$got" ]]; then
+        echo "error: $msg (expected=$expected, got=$got)"
+    else
+        echo "ok: $msg"
+    fi
+}
+
+function assert_file_size()
+{
+    local subbuf_count=$1
+    local filepath=$2
+    local msg
+    local expected
+    local got
+
+    msg="File size should be equal to the number of sent messages"
+    expected=$(("$subbuf_count" * $SUBBUF_SIZE * $EVENT_SIZE))
+    got=$(ls -l "$filepath" | awk '{print $5}')
+
+    assert_eq "$msg" "$expected" "$got"
+}
 
 function send_message() {
     local subbuf=$(($1 % 256))
@@ -30,7 +59,7 @@ function run_stress_test() {
 }
 
 function show_usage() {
-    echo "$SCRIPT_NAME SUBBUF_COUNT [--check FILE]"
+    echo "$SCRIPT_NAME SUBBUF_COUNT [FILE]"
 }
 
 if [[ $# -eq "1" ]]; then
@@ -38,8 +67,9 @@ if [[ $# -eq "1" ]]; then
     exit 0
 fi
 
-if [[ $# -eq "3" ]]; then
-    # TODO: add some sort of assertion code
+if [[ $# -eq "2" ]]; then
+    assert_file_size "$1" "$2"
+    # TODO: add more assertions
     exit 0
 fi
 
